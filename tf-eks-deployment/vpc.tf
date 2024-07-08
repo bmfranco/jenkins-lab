df55c85476a14eef56cba0f1074724a72eb21af4
bmfranco <128904191+bmfranco@users.noreply.github.com>
2024-07-07 22:08:56 -0300

    Update vpc.tf

--- tf-eks-deployment/vpc.tf
+++ tf-eks-deployment/vpc.tf
@@ -14,16 +14,16 @@ module "my-vpc" {
   enable_dns_hostnames = true
 
   tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/cluster2" = "shared"
   }
 
   public_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/cluster2" = "shared"
     "kubernetes.io/role/elb"                  = 1
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/my-eks-cluster" = "shared"
+    "kubernetes.io/cluster/cluster2" = "shared"
     "kubernetes.io/role/internal-elb"         = 1
   }
 }
